a6d57fef85edc622ec8f6e15b3fa7e04bb78a9d2
Manav Ramesh Kokne <koknemanav@gmail.com>
2024-10-18 20:12:47 +0530

    2nd Commit

---
+++ studentLogin.js
@@ -0,0 +1,15 @@
+console.log("aaaaaaa");
+const passwordInput = document.getElementById("password");
+const passwordVisibilityImg = document.getElementById("pwd-visibility-img");
+
+passwordVisibilityImg.addEventListener("click", () => {
+    const isPasswordVisible = passwordInput.type === "text";
+
+    if (isPasswordVisible) {
+        passwordVisibilityImg.setAttribute("src", "eye-closed.svg");
+        passwordInput.setAttribute("type", "password");
+    } else {
+        passwordVisibilityImg.setAttribute("src", "eye-open.svg");
+        passwordInput.setAttribute("type", "text");
+    }
+});
